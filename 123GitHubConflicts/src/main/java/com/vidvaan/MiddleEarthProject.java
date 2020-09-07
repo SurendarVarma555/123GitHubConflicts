90433404a12a8a734ff339575a4d0e107d79b025
SurendarVarma555 <batthulasurendar@gmail.com>
2020-09-07 13:11:54 +0530

    ConflictsProject

---
+++ 123GitHubConflicts/src/main/java/com/vidvaan/MiddleEarthProject.java
@@ -0,0 +1,11 @@
+package com.vidvaan;
+
+public class MiddleEarthProject {
+	public static void main(String[] args) {
+		String[] names= {"Somu","Ramu","Gogesh"};
+		for (int i = 0; i < names.length; i++) {
+			System.out.format("Name: %s\n", names[i]);
+		}
+	}
+
+}
