5236dc42882ce4cfc274bc39ccdb3e3476d1f1dc
SurendarVarma555 <batthulasurendar@gmail.com>
2020-09-07 12:48:44 +0530

    WH-"Merge Conflicts"

---
+++ 5555MergeConflictsResolving/src/com/vidvaan/HelloMiddleEarth.java
@@ -0,0 +1,13 @@
+package com.vidvaan;
+
+public class HelloMiddleEarth {
+
+	public static void main(String[] args) {
+		String[] names= {"Ram","Somu","Ganesh"};
+		for (int i = 0; i < names.length; i++) {
+			System.out.format("Name: %s!\n", names[i]);
+			
+		}
+	}
+
+}
